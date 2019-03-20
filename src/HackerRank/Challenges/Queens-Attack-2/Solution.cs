8040870a3fa23c680d2e00bf1fcd923b1345f5a6
Rad Danesh <radd@pressreader.com>
2019-03-20 09:47:35 -0700

    Added queens attack solution

---
+++ src/HackerRank/Challenges/Queens-Attack-2/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HackerRank
+{
+    public static partial class Solution
+    {
+        public static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles)
+        {
+            var topLeft = c_q - Math.Min(n - r_q, c_q - 1);
+            var top = n;
+            var topRight = r_q + (n - Math.Max(r_q, c_q));
+            var right = n;
+            var buttomRight = c_q + Math.Min(r_q - 1, n - c_q);
+            var buttom = 1;
+            var buttomLeft = r_q - (Math.Min(r_q, c_q) - 1);
+            var left = 1;
+
+            foreach (var obstacle in obstacles)
+            {
+                if (obstacle[0] == r_q)
+                {
+                    if (obstacle[1] < c_q && obstacle[1] > left)
+                        left = obstacle[1] + 1;
+                    else if (obstacle[1] > c_q && obstacle[1] < right)
+                        right = obstacle[1] - 1;
+                }
+                else if (obstacle[1] == c_q)
+                {
+                    if (obstacle[0] < r_q && obstacle[0] > buttom)
+                        buttom = obstacle[0] + 1;
+                    else if (obstacle[0] > r_q && obstacle[0] < top)
+                        top = obstacle[0] - 1;
+                }
+                else if (obstacle[0] < r_q && obstacle[1] < c_q
+                         && r_q - obstacle[0] == c_q - obstacle[1]
+                         && buttomLeft < obstacle[0] + 1)
+                {
+                    buttomLeft = obstacle[0] + 1;
+                }
+                else if (obstacle[0] > r_q && obstacle[1] > c_q
+                         && obstacle[0] - r_q == obstacle[1] - c_q
+                         && topRight > obstacle[0] - 1)
+                {
+                    topRight = obstacle[0] - 1;
+                }
+                else if (obstacle[0] < r_q && obstacle[1] > c_q
+                         && r_q - obstacle[0] == obstacle[1] - c_q
+                         && buttomRight > obstacle[1] - 1)
+                {
+                    buttomRight = obstacle[1] - 1;
+                }
+                else if (obstacle[0] > r_q && obstacle[1] < c_q
+                         && obstacle[0] - r_q == c_q - obstacle[1]
+                         && topLeft < obstacle[1] + 1)
+                {
+                    topLeft = obstacle[1] + 1;
+                }
+            }
+
+            return top - buttom
+                   + right - left
+                   + topRight - buttomLeft
+                   + buttomRight - topLeft;
+        }
+    }
+}
---
+++ src/HackerRankTest/Challenges/Queens-Attack-2/SolutionTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using HackerRank;
+using Xunit;
+
+namespace HackerRankTest
+{
+    public partial class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(QueensAttackData))]
+        public void queensAttackTest(int n, int k, int r_q, int c_q, int[][] obstacles, int expectedOutput)
+        {
+            // Act
+            var actualOutput = Solution.queensAttack(n, k, r_q, c_q, obstacles);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        public static IEnumerable<object[]> QueensAttackData =>
+            new List<object[]>
+            {
+                new object[] { 88587, 9, 20001, 20003,
+                    new[] {
+                        new[] { 20001, 20002 },
+                        new[] { 20001, 20004 },
+                        new[] { 20000, 20003 },
+                        new[] { 20002, 20003 },
+                        new[] { 20000, 20004 },
+                        new[] { 20000, 20002 },
+                        new[] { 20002, 20004 },
+                        new[] { 20002, 20002 },
+                        new[] { 564, 323 }
+                    }, 0 },
+                new object[] { 5, 3, 4, 3,
+                    new[]
+                    {
+                        new[] { 5, 5 },
+                        new[] { 4, 2 },
+                        new[] { 2, 3 }
+                    }, 10 },
+                new object[] { 1, 0, 1, 1, new int[0][], 0 },
+                new object[] { 4, 0, 4, 4, new int[0][], 9 }
+            };
+    }
+}
