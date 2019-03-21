a927c30dd702e3a1772c97d8f7b91f8083ef2bff
Rad Danesh <radd@pressreader.com>
2019-03-20 17:08:04 -0700

    Added activity notifications solution

---
+++ src/HackerRank/Challenges/Fraudulent-Activity-Notifications/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public static partial class Solution
+    {
+        public static int activityNotifications(int[] expenditure, int d)
+        {
+            var ans = 0;
+            var freq = new Dictionary<int, int>();
+
+            for (var i = 0; i < expenditure.Length; i++)
+            {
+                if (i >= d)
+                {
+                    var m = d / 2;
+                    var r = d % 2;
+
+                    var m1 = FindMedian(freq, m + r);
+
+                    var m2 = r == 1 
+                        ? m1 
+                        : FindMedian(freq, m + 1);
+
+                    if (expenditure[i] >= m1 + m2)
+                        ans++;
+
+                    freq[expenditure[i - d]]--;
+                }
+
+                if (freq.ContainsKey(expenditure[i]))
+                    freq[expenditure[i]]++;
+                else
+                    freq.Add(expenditure[i], 1);
+            }
+
+            return ans;
+        }
+
+        private static int FindMedian(IDictionary<int, int> freq, int index)
+        {
+            var sum = 0;
+            var result = 0;
+
+            while (result <= 200 && sum < index)
+            {
+                if (freq.TryGetValue(result++, out var f))
+                    sum += f;
+            }
+
+            return result - 1;
+        }
+    }
+}
---
+++ src/HackerRankTest/Challenges/Fraudulent-Activity-Notifications/SolutionTest.cs
@@ -0,0 +1,21 @@
+﻿using HackerRank;
+using Xunit;
+
+namespace HackerRankTest
+{
+    public partial class SolutionTest
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 4, 2, 3, 6, 8, 4, 5 }, 5, 2)]
+        [InlineData(new[] { 1, 2, 3, 4, 4 }, 4, 0)]
+        [InlineData(new[] { 10, 20, 30, 40, 50 }, 3, 1)]
+        public void activityNotificationsTest(int[] expenditure, int d, int expectedOutput)
+        {
+            // Act
+            var actualOutput = Solution.activityNotifications(expenditure, d);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
