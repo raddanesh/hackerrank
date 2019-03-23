5d6340e0defe2005ad5892724aabad037e57c67a
Rad Danesh <radd@pressreader.com>
2019-03-23 10:44:03 -0700

    Added crush solution

---
+++ src/HackerRank/Challenges/Crush/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace HackerRank
+{
+    public static partial class Solution
+    {
+        /// <summary>
+        /// Return an integer, the maximum value in the resulting array.
+        /// https://www.hackerrank.com/challenges/crush/problem
+        /// </summary>
+        /// <param name="n">The number of elements in your array</param>
+        /// <param name="queries">A two dimensional array of queries where each queries[i] contains three integers, a, b, and k.</param>
+        public static long arrayManipulation(int n, int[][] queries)
+        {
+            var arr = new long[n];
+            long ans = 0;
+            long tmp = 0;
+
+            foreach (var q in queries)
+            {
+                arr[q[0] - 1] += q[2];
+                if (q[1] < n)
+                    arr[q[1]] -= q[2];
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                tmp += arr[i];
+                if (tmp > ans)
+                    ans = tmp;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ src/HackerRankTest/Challenges/Crush/SolutionTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using HackerRank;
+using Xunit;
+
+namespace HackerRankTest
+{
+    public partial class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(ArrayManipulationData))]
+        public void arrayManipulationTest(int n, int[][] queries, long expectedOutput)
+        {
+            // Act
+            var actualOutput = Solution.arrayManipulation(n, queries);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        public static IEnumerable<object[]> ArrayManipulationData =>
+            new List<object[]>
+            {
+                new object[] { 10,
+                    new[] {
+                        new[] { 1, 5, 3 },
+                        new[] { 4, 8, 7 },
+                        new[] { 6, 9, 1 }
+                    }, 10},
+                new object[] { 5,
+                    new[] {
+                        new[] { 1, 2, 100 },
+                        new[] { 2, 5, 100 },
+                        new[] { 3, 4, 100 }
+                    }, 200},
+            };
+    }
+}
