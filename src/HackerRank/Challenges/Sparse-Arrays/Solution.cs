fe3b81ed92645232c319c08a1e052ac373f9de10
Rad Danesh <radd@pressreader.com>
2019-03-22 22:15:24 -0700

    Added sparse arrays solution

---
+++ src/HackerRank/Challenges/Sparse-Arrays/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public static partial class Solution
+    {
+        /// <summary>
+        /// Return an array of integers representing the frequency of occurrence of each query string in strings.
+        /// https://www.hackerrank.com/challenges/sparse-arrays/problem
+        /// </summary>
+        /// <param name="strings">An array of strings to search</param>
+        /// <param name="queries">An array of query strings</param>
+        public static int[] matchingStrings(string[] strings, string[] queries)
+        {
+            var dic = new Dictionary<string, int>();
+            var ans = new int[queries.Length];
+
+            foreach (var s in strings)
+            {
+                if (dic.ContainsKey(s))
+                    dic[s]++;
+                else
+                    dic[s] = 1;
+            }
+
+            for (var i = 0; i < queries.Length; i++)
+            {
+                if (!dic.TryGetValue(queries[i], out ans[i]))
+                    ans[i] = 0;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ src/HackerRankTest/Challenges/Sparse-Arrays/SolutionTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using HackerRank;
+using Xunit;
+
+namespace HackerRankTest
+{
+    public partial class SolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(MatchingStringsData))]
+        public void matchingStringsTest(string[] strings, string[] queries, int[] expectedOutput)
+        {
+            // Act
+            var actualOutput = Solution.matchingStrings(strings, queries);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        public static IEnumerable<object[]> MatchingStringsData =>
+            new List<object[]>
+            {
+                new object[] { 
+                    new[] { "def", "de", "fgh" },
+                    new[] { "de", "lmn", "fgh" },
+                    new[] { 1, 0, 1 }
+                },
+                new object[] {
+                    new[] {
+                        "abcde",
+                        "sdaklfj",
+                        "asdjf",
+                        "na",
+                        "basdn",
+                        "sdaklfj",
+                        "asdjf",
+                        "na",
+                        "asdjf",
+                        "na",
+                        "basdn",
+                        "sdaklfj",
+                        "asdjf"
+                    },
+                    new[] {
+                        "abcde",
+                        "sdaklfj",
+                        "asdjf",
+                        "na",
+                        "basdn"
+                    },
+                    new [] { 1, 3, 4, 3, 2 }
+                }
+            };
+    }
+}
